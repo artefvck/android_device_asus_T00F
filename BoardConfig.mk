42407fe2518b77f3066d8d44f7cce86791ae2bc9
artefvck <artefvck07@gmail.com>
2016-06-18 00:55:02 +0700

    initial import
    Change-Id: I08a7e284c8675b63ac9c75298de8552b6bae58c0

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_ARCH := x86
+TARGET_ARCH_VARIANT := x86-atom
+TARGET_CPU_ABI := x86
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_CPU_ABI_LIST := x86,armeabi-v7a,armeabi
+TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
+TARGET_BOARD_PLATFORM := clovertrail
+TARGET_BOOTLOADER_BOARD_NAME := clovertrail
+TARGET_CPU_VARIANT := x86
+
+
+# Use the non-open-source parts, if they're present
+-include vendor/asus/T00F/BoardConfigVendor.mk
