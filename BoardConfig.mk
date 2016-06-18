b09b42061ec2e2da68fd2e9178509b637aa6d621
artefvck <artefvck07@gmail.com>
2016-06-18 17:01:41 +0700

    T00F : set permissive
    Change-Id: Ia0d208dbbf47d6aaf9f0da971ebd400e4f0e5310

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
 
 # Kernel cmdline
 BOARD_KERNEL_CMDLINE := init=/init pci=noearly earlyprintk=nologger loglevel=0 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=redhookbay watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on vmalloc=172M slub_max_order=2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # OTA Packaging / Bootimg creation
 BOARD_CUSTOM_BOOTIMG := true
